b0de2c4ae028433797fd180133242cb571cda29e
OptimusMechanic <15827429720@163.com>
2026-05-17 17:32:11 +0800

    主窗体

---
+++ Workspace_F.Mech/Workspace_F.Mech/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Workspace_F.Mech
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
